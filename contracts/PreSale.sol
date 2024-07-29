bb48c53b8e611250e9a29b68bf064b2c23bb227f
Khaledshaikh2244 <khankhaled501@gmail.com>
2024-07-29 21:40:36 +0530

    completed Token layout

--- contracts/PreSale.sol
+++ contracts/PreSale.sol
@@ -7,7 +7,11 @@ pragma solidity ^0.8.0;
 
 
 interface IERC20 {
-    function transfer() 
+    function transfer(address recipient, uint256 amount) external returns(bool);
+    function balanceOf(address account) external view returns(uint256);
+    function symbol() external view returns (string memory);
+    function totalSupply() external view returns(uint256);
+    function name() external view returns(string memory);
 }
 
 contract ICOMarketplace{
@@ -15,28 +19,62 @@ contract ICOMarketplace{
 // tokenDetails
 
 struct TokenDetails {
-
+    address token;
+    bool supported;
+    uint256 price;
+    address creator;
+    string name;
+    string symbool;
 }
     
 // outer layout
+(
+mapping(address => TokenDetails) public tokendetails;
+address [] public allSupportedTokens;
+address public owner;
+
+
+// Events
+
+event TokenRecieved(address indexed token, address indexed from, uint256 amount);
+event TokenTransferred(address indexed token, address indexed to, uint256 amount);
+event TokenWithDraw(address indexed token, address indexed to, uint256);
+event TokenAdded(address indexed token,  uint256 indexed price, address indexed creator ,  string indexed symbol, string indexed name)
 
 // Modifiers
 
-modifier supportedToken(){}
+modifier supportedToken(address _token){
+    require(tokenDetails[_token].supported == true , "Token is not supported");
+    _;
+}
 
-modifier onlyOwner() {}
+modifier onlyOwner(address _owner) {
+    require(msg.sender == owner, "Caller is not the owner");
+    _;
+}
 
-modifier onlyCreater() {}
+modifier onlyCreater(address _creator) {
+    require(msg.sender == tokenDetails[_token].creator, "Caller is not the token creator");
+    _;
+}
 
 
- recieve() external payable{}
+ recieve () external payable{
+    revert("Contract does not supported ehter directly");
+ }
 
- constructor() {}
+ constructor() {
+    owner = msg.sender;
+ }
 
 //  functions
 
 // contract level fnc
-function createICOSale(address _token, uint256 _price) {}
+function createICOSale(address _token, uint256 _price) external{
+    IERC20 token = IERC20(_token);
+    string memory tokenName = token.name();
+    string memory tokenSymbol = token.symbol()
+}
 
 
 // handling overflow$underflow
@@ -65,7 +103,7 @@ function withdraw(address _token , uint256 amount) external onlyCreater(_token) 
 function getTokenDetails(address _token) external view returns(TokenDetails[] memory) {}
 
 // get token created bu specifi users
-function getCreatedBy(address _creator) external view returns (TokenDetaisls[] memory) {}
+function getCreatedBy(address _creator) external view returns (TokenDetails[] memory) {}
 
 function getAllTokens()  external view returns (TokenDetails[] memory) {}
 
