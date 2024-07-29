711b3b2bf6893907d8492a0f180a8bf59719ac56
Khaledshaikh2244 <khankhaled501@gmail.com>
2024-07-29 12:40:12 +0530

    added token layout

--- contracts/PreSale.sol
+++ contracts/PreSale.sol
@@ -7,6 +7,7 @@ pragma solidity ^0.8.0;
 
 
 interface IERC20 {
+    function transfer() 
 }
 
 contract ICOMarketplace{
@@ -69,74 +70,6 @@ function getCreatedBy(address _creator) external view returns (TokenDetaisls[] m
 function getAllTokens()  external view returns (TokenDetails[] memory) {}
 
 }
-
-}
-
-
-
-
-
-// SPDX-License-Identifier: MIT
-
-pragma solidity ^0.8.0;
-
-// interface of erc20 token
-//  2 ways =>  frontend , within smart contract
-
-
-interface IERC20 {
-}
-
-contract ICOMarketplace{
-
-// tokenDetails
-
-struct TokenDetails {
-
-}
-    
-// outer layout
-
-// Modifiers
-
-modifier supportedToken(){}
-
-modifier onlyOwner() {}
-
-modifier onlyCreater() {}
-
-
-recieve() external payable{}
-
- constructor() {}
-
-//  functions
-
-// contract level fnc
-function createICOSale(address _token, uint256 _price) {}
-
-
-// handling overflow$underflow
-function multiply(uint256 x, uint256 y ) internal returns (uint256 z) {}
-
-
-// buyToken
-
-// amount = how many token
-// price = on current token price
-function buyToken(address _token, uint256 _amount)  supportedToken(_token){
-}
-
-// getting balance
- function getBalance(uint _token) external view returns(uint256){}
-
-
-// returning list of supported token
- function getSupportedToken() view public returns (type name) {
-    
- } 
-}
-
 
 
 
