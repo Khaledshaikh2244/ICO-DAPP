cda340e04205151c8a4f76d6b951726249700fef
Khaledshaikh2244 <khankhaled501@gmail.com>
2024-08-05 16:51:17 +0530

    created context

--- pages/_app.js
+++ pages/_app.js
@@ -1,4 +1,10 @@
+import Head from "next/head";
+import toast from "react-hot-toast";
 import "../styles/globals.css";
+
+
+// INTERNAL IMPORTS
+import {StateContextProvider} from "../Context/index.js"
 
 export default function App({ Component, pageProps }) {
   return (
