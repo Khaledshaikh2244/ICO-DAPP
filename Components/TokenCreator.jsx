9a8dbd6e50df691d1e2be8614f3c8f949c60af12
Khaledshaikh2244 <khankhaled501@gmail.com>
2024-08-26 15:36:05 +0530

    imported props into TokenCreator

--- Components/TokenCreator.jsx
+++ Components/TokenCreator.jsx
@@ -1,6 +1,15 @@
 import React from "react";
 
-const TokenCreator = () => {
+const TokenCreator = ({
+  createERC,
+  shortenAddress,
+  openTokenCreator,
+  setLoader,
+  address,
+  connectWallet ,
+  PINATA_API_KEY ,
+  PINATA_SECRET_KEY,
+}) => {
   return <div>TokenCreator</div>;
 };
 
