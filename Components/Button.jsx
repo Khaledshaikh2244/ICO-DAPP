5359ce7ec12708f3e710fc201054f10bb0e3381e
Khaledshaikh2244 <khankhaled501@gmail.com>
2024-08-26 18:37:51 +0530

    fixed naming isssue

--- Components/Button.jsx
+++ Components/Button.jsx
@@ -3,8 +3,8 @@ import React from "react";
 // getting props 
 // dynamic class for specific btn
 
-const Button = ({name, handleCLick, classStyle}) => {
-  return <button className= {`${classStyle} new-button` } onClick={handleCLick}>{name}</button>
+const Button = ({name, handleClick, classStyle}) => {
+  return <button className= {`${classStyle} new-button` } onClick={handleClick}>{name}</button>
 };
 
 export default Button;
